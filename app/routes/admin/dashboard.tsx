c1789027bba2d27e1794c93dc04ecfddc191acf7
hakimxiao <alhakimabusolihin@gmail.com>
2025-09-23 20:08:54 +0700

    - CREATE UI DAN COMPONENT -
    1. Membuat component TripCard yang didalamnya merender Sebuah Component dari ** SyncFusion yaitu chip **
    2. Memanggil tripcard tadi kedalaman UI dashboard.

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,27 +1,15 @@
 import {Header, StateCard, TripCard} from "components";
+import {user, dashboardStats, allTrips} from "~/constants"
+
+const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
 
 const Dashboard = () => {
-  const user = { name: "Hakim" };
-  const dashboardStats = {
-      totalUsers: 12450,
-      usersJoined: {
-          currentMonth: 218,
-          lastMonth: 170
-      },
-      totalTrips: 3210,
-      tripsCreated: { currentMonth: 150, lastMonth: 250 },
-      userRole: { total: 62, currentMonth: 25, lastMonth: 15 }
-  }
-
-  const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
-
   return (
     <main className="dashboard wrapper">
       <Header
         title={`Welcome ${user?.name ?? "Guest"} 👋 `}
         description="Lacak aktivitas, trend dan tempat populer secara real time"
       />
-
         <section className="flex flex-col gap-6">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
 
@@ -45,7 +33,24 @@ const Dashboard = () => {
                 />
             </div>
         </section>
-        <TripCard />
+        <section className="container">
+            <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+
+            <div className="trip-grid">
+                {allTrips.slice(0, 4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+                    <TripCard
+                        key={id}
+                        id={id.toString()}
+                        name={name}
+                        imageUrl={imageUrls[0]}
+                        location={itinerary?.[0]?.location ?? ""}
+                        tags={tags}
+                        price={estimatedPrice}
+                    />
+                ))}
+            </div>
+        </section>
+        {/*<TripCard />*/}
     </main>
   );
 };
