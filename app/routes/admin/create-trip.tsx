94e80512579d37fa9c76a08d80dfdbd843165896
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-02 14:52:44 +0700

    - MENYELESAIKAN FUNGSIONALITAS UI CREATE-TRIP -
    * Disini kita menyelesaikan semua fungsionalitasnya dan sudah sesuai hanya saja di next kita akan mengatur handle Submit reall nya, dimana kita akan memanfaatkan ai untuk membuat trip

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -3,9 +3,11 @@ import {Header} from "../../../components";
 import {ComboBoxComponent} from "@syncfusion/ej2-react-dropdowns";
 import type {Route} from "./+types/create-trip"
 import {comboBoxItems, selectItems} from "~/constants";
-import {formatKey} from "~/lib/utils";
+import {cn, formatKey} from "~/lib/utils";
 import {LayerDirective, LayersDirective, MapsComponent} from "@syncfusion/ej2-react-maps";
 import {world_map} from "~/constants/world_map";
+import {ButtonComponent} from "@syncfusion/ej2-react-buttons";
+import {account} from "~/appwrite/client";
 
 export const loader = async() => {
     const response = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,latlng,maps");
@@ -24,7 +26,6 @@ export const loader = async() => {
 
 const CreateTrip = ({loaderData}: Route.ComponentProps) => {
     const countries = loaderData as Country[];
-    console.log("countries", countries);
     const [formData, setFormData] = useState<TripFormData>({
         country: "",
         travelStyle: "",
@@ -33,7 +34,9 @@ const CreateTrip = ({loaderData}: Route.ComponentProps) => {
         duration: 0,
         groupType: ""
     });
-    console.log("formData", formData);
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+
 
     const countryData = countries.map((country) => (
         {
@@ -49,7 +52,39 @@ const CreateTrip = ({loaderData}: Route.ComponentProps) => {
         coordinates: countries.find((c: Country) => c.text === formData.country)?.coordinates || []
     }]
 
-    const handleSubmit = async() => {}
+    const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+
+        if(!formData.country || !formData.travelStyle || !formData.interest || !formData.budget || !formData.groupType) {
+            setError("Please provide values for all fields");
+            setLoading(false);
+            return;
+        }
+
+        if(formData.duration < 1 || formData.duration > 10) {
+            setError("Duration must be between 1 and 10 days");
+            setLoading(false);
+            return;
+        }
+
+        const user = await account.get();
+        if(!user.$id) {
+            console.error("User not authenticated");
+            setLoading(false);
+            return;
+        }
+
+        try {
+            console.log(user);
+            console.log(formData);
+        } catch (e) {
+            console.error("Error generating trip", e);
+        } finally {
+            setLoading(false);
+        }
+    }
+
     const handleChange = async(key: keyof TripFormData, value: string | number) => {
         setFormData({ ...formData, [key]: value });
     }
@@ -132,6 +167,7 @@ const CreateTrip = ({loaderData}: Route.ComponentProps) => {
                         </div>
                     ))}
 
+                    {/* BAGIAN FAV SAYA */}
                     <div>
                         <label htmlFor="location">Location on the world map</label>
                         <MapsComponent>
@@ -146,6 +182,23 @@ const CreateTrip = ({loaderData}: Route.ComponentProps) => {
                             </LayersDirective>
                         </MapsComponent>
                     </div>
+
+                    <div className="bg-gray-200 h-px w-full" />
+
+                    {error && <div className="error">
+                        <p>{error}</p>
+                    </div>}
+
+                    <footer className="px-6 w-full">
+                        <ButtonComponent type="submit" className="button-class !h-12 !w-full" disabled={loading}>
+                            <img src={`/assets/icons/${loading ? 'loader.svg' : 'magic-star.svg'}`} alt="icon" className={cn("size-5", {
+                                "animate-spin": loading
+                            })}/>
+                            <span className="p-16-semibold text-white">
+                                {loading ? "Generating..." : "Generate Trip"}
+                            </span>
+                        </ButtonComponent>
+                    </footer>
                 </form>
             </section>
         </main>
