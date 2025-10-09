591230414aa63b7e7fa3affecc1cdd08668c0dc4
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-09 11:06:44 +0700

    - SUB FINISHING -
    * Memggunakan data asli didalam dashboard.tsx pada gravik dan juga travel.
    
    * pada bagian dashboard.ts wajib anda pelajari lagi sebab ini cuma copas, dan fungsi ini bagus karena di implement filter reduce dll.

--- app/appwrite/dashboard.ts
+++ app/appwrite/dashboard.ts
@@ -1,23 +1,24 @@
-import { appwriteConfig, database } from "~/appwrite/client";
+import { parseTripData } from "~/lib/utils";
+import { database, appwriteConfig } from "./client";
 
 interface Document {
-    [key: string]: any
+    [key: string]: any;
 }
 
 type FilterByDate = (
-    items: Document[],
+items: Document[],
     key: string,
     start: string,
-    end?: string,
+    end?: string
 ) => number;
 
 export const getUsersAndTripsStats = async (): Promise<DashboardStats> => {
     const d = new Date();
     const startCurrent = new Date(d.getFullYear(), d.getMonth(), 1).toISOString();
-    const startPrev = new Date(d.getFullYear(), d.getMonth() - 1, 1).toISOString();
+    const startPrev = new Date(d.getFullYear(), d.getMonth() -1, 1).toISOString();
     const endPrev = new Date(d.getFullYear(), d.getMonth(), 0).toISOString();
 
-    const [ users, trips ] = await Promise.all([
+    const [users, trips] = await Promise.all([
         database.listDocuments(
             appwriteConfig.databaseId,
             appwriteConfig.userCollectionId
@@ -46,11 +47,11 @@ export const getUsersAndTripsStats = async (): Promise<DashboardStats> => {
                 undefined
             ),
             lastMonth: filterByDate(
-                    users.documents,
-                    'joinedAt',
-                    startPrev,
-                    endPrev
-                )
+                users.documents,
+                'joinedAt',
+                startPrev,
+                endPrev
+            )
         },
         userRole: {
             total: filterUsersByRole('user').length,
@@ -60,13 +61,12 @@ export const getUsersAndTripsStats = async (): Promise<DashboardStats> => {
                 startCurrent,
                 undefined
             ),
-            lastMonth:
-                filterByDate(
-                    users.documents,
-                    'joinedAt',
-                    startPrev,
-                    endPrev
-                )
+            lastMonth: filterByDate(
+                filterUsersByRole('user'),
+                'joinedAt',
+                startPrev,
+                endPrev
+            )
         },
         totalTrips: trips.total,
         tripsCreated: {
@@ -76,13 +76,87 @@ export const getUsersAndTripsStats = async (): Promise<DashboardStats> => {
                 startCurrent,
                 undefined
             ),
-            lastMonth:
-                filterByDate(
-                    trips.documents,
-                    'createdAt',
-                    startPrev,
-                    endPrev
-                )
-        }
+            lastMonth: filterByDate(
+                filterUsersByRole('user'),
+                'joinedAt',
+                startPrev,
+                endPrev
+            )
+        },
     }
 }
+
+export const getUserGrowthPerDay = async () => {
+    const users = await database.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId
+    );
+
+    const userGrowth = users.documents.reduce(
+        (acc: { [key: string]: number }, user: Document) => {
+            const date = new Date(user.joinedAt);
+            const day = date.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+            });
+            acc[day] = (acc[day] || 0) + 1;
+            return acc;
+        },
+        {}
+    );
+
+    return Object.entries(userGrowth).map(([day, count]) => ({
+        count: Number(count),
+        day,
+    }));
+};
+
+export const getTripsCreatedPerDay = async () => {
+    const trips = await database.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.tripCollectionId
+    );
+
+    const tripsGrowth = trips.documents.reduce(
+        (acc: { [key: string]: number }, trip: Document) => {
+            const date = new Date(trip.createdAt);
+            const day = date.toLocaleDateString("en-US", {
+                month: "short",
+                day: "numeric",
+            });
+            acc[day] = (acc[day] || 0) + 1;
+            return acc;
+        },
+        {}
+    );
+
+    return Object.entries(tripsGrowth).map(([day, count]) => ({
+        count: Number(count),
+        day,
+    }));
+};
+
+export const getTripsByTravelStyle = async () => {
+    const trips = await database.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.tripCollectionId
+    );
+
+    const travelStyleCounts = trips.documents.reduce(
+        (acc: { [key: string]: number }, trip: Document) => {
+            const tripDetail = parseTripData(trip.tripDetails);
+
+            if (tripDetail && tripDetail.travelStyle) {
+                const travelStyle = tripDetail.travelStyle;
+                acc[travelStyle] = (acc[travelStyle] || 0) + 1;
+            }
+            return acc;
+        },
+        {}
+    );
+
+    return Object.entries(travelStyleCounts).map(([travelStyle, count]) => ({
+        count: Number(count),
+        travelStyle,
+    }));
+};
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,16 +1,42 @@
-import {Header, StateCard, TripCard} from "components";
-import {user, dashboardStats, allTrips} from "~/constants"
-import {getUser} from "~/appwrite/auth";
+import {Header, StatsCard, TripCard} from "components";
+import {getAllusers, getUser} from "~/appwrite/auth";
 import type { Route } from "./+types/dashboard";
+import {getTripsByTravelStyle, getUserGrowthPerDay, getUsersAndTripsStats} from "~/appwrite/dashboard";
+import {getAllTrips} from "~/appwrite/trips";
+import {parseTripData} from "~/lib/utils";
 
-const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
 
-// ini seperti useEFfect : dia akan jijalankan sebelum komponen di render
-export const clientLoader = async () => await getUser();
+export const clientLoader = async () => {
+    const [user, dashboardStats, trips, userGrowth, tripsByTravelStyle, allUsers] = await Promise.all([
+        await getUser(),
+        await getUsersAndTripsStats(),
+        await getAllTrips(4, 0),
+        await getUserGrowthPerDay(),
+        await getTripsByTravelStyle(),
+        await getAllusers(4, 0)
+    ])
+
+    const allTrips = trips.allTrips.map(({ $id, tripDetails, imageUrls }) => ({
+        id: $id,
+        ...parseTripData(tripDetails),
+        imageUrls: imageUrls ?? []
+    }))
+
+    const mappedUsers: UsersItineraryCount[] = allUsers.users.map((user) => ({
+        imageUrl: user.imageUrl,
+        name: user.name,
+        count: user.itinerary.count
+    }))
+
+    return {
+        user, dashboardStats, allTrips, userGrowth, tripsByTravelStyle, allUsers: mappedUsers
+    }
+}
 
 const Dashboard = ({ loaderData }: Route.ComponentProps ) => {
 
-    const user = loaderData as User | null;
+    const user = loaderData.user as User | null;
+    const { dashboardStats, allTrips, userGrowth, tripsByTravelStyle, allUsers } = loaderData
 
     return (
     <main className="dashboard wrapper">
@@ -21,23 +47,23 @@ const Dashboard = ({ loaderData }: Route.ComponentProps ) => {
         <section className="flex flex-col gap-6">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
 
-                <StateCard
+                <StatsCard
                     headerTitle="Total Users"
-                    total={totalUsers}
-                    currentMonthCount={usersJoined.currentMonth}
-                    lastMonthCount={usersJoined.lastMonth}
+                    total={dashboardStats.totalUsers}
+                    currentMonthCount={dashboardStats.usersJoined.currentMonth}
+                    lastMonthCount={dashboardStats.usersJoined.lastMonth}
                 />
-                <StateCard
+                <StatsCard
                     headerTitle="Total Trips"
-                    total={totalTrips}
-                    currentMonthCount={tripsCreated.currentMonth}
-                    lastMonthCount={tripsCreated.lastMonth}
+                    total={dashboardStats.totalTrips}
+                    currentMonthCount={dashboardStats.tripsCreated.currentMonth}
+                    lastMonthCount={dashboardStats.tripsCreated.lastMonth}
                 />
-                <StateCard
+                <StatsCard
                     headerTitle="Active Users"
-                    total={userRole.total}
-                    currentMonthCount={userRole.currentMonth}
-                    lastMonthCount={userRole.lastMonth}
+                    total={dashboardStats.userRole.total}
+                    currentMonthCount={dashboardStats.userRole.currentMonth}
+                    lastMonthCount={dashboardStats.userRole.lastMonth}
                 />
             </div>
         </section>
@@ -45,15 +71,15 @@ const Dashboard = ({ loaderData }: Route.ComponentProps ) => {
             <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
 
             <div className="trip-grid">
-                {allTrips.slice(0, 4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+                {allTrips.map((trip) => (
                     <TripCard
-                        key={id}
-                        id={id.toString()}
-                        name={name}
-                        imageUrl={imageUrls[0]}
-                        location={itinerary?.[0]?.location ?? ""}
-                        tags={tags}
-                        price={estimatedPrice}
+                        key={trip.id}
+                        id={trip.id.toString()}
+                        name={trip.name!}
+                        imageUrl={trip.imageUrls[0]}
+                        location={trip.itinerary?.[0]?.location ?? ""}
+                        tags={[trip.interests!, trip.travelStyle!]}
+                        price={trip.estimatedPrice!}
                     />
                 ))}
             </div>
