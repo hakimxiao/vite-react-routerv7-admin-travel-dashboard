1b1ea075cb27e2042cae1e9132e1aab542a9f45b
hakimxiao <alhakimabusolihin@gmail.com>
2025-10-10 13:56:15 +0700

    - FINAL ACTION -
    - MEMBUAT CHART DENGAN SYNCFUSION -
    
    * Waw sangat luarbiasa sekali kita dapat membuat sebuah grafik atau chart denganbn sangat mudah dengan menggunakan syncfusion.
    
    * Penggunaannya juga sangat simple, Mendeklarasikan inject (Apa saja yang akan kita cantumkan di chart) kemudian membuat datanya dengan SeriesCollectionDirective kemudian mendeklarasikan nya di SeriesDirective
    
    * Kemudian menginclude datanya mudah tinggal pasang variabel nya di dataSource pada SeriesDirective, kemudian deklarasikan field yang mau dipakai misal xName="day" // x name : diisi dengan userGrowth.day
    
    * Membuat grid menampilkan info user dan trip di dashboard

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -4,6 +4,16 @@ import type { Route } from "./+types/dashboard";
 import {getTripsByTravelStyle, getUserGrowthPerDay, getUsersAndTripsStats} from "~/appwrite/dashboard";
 import {getAllTrips} from "~/appwrite/trips";
 import {parseTripData} from "~/lib/utils";
+import {
+    Category,
+    ChartComponent,
+    ColumnSeries,
+    DataLabel, SeriesCollectionDirective, SeriesDirective,
+    SplineAreaSeries,
+    Tooltip
+} from "@syncfusion/ej2-react-charts";
+import {ColumnDirective, ColumnsDirective, GridComponent, Inject} from "@syncfusion/ej2-react-grids";
+import {tripXAxis, tripyAxis, userXAxis, useryAxis} from "~/constants";
 
 
 export const clientLoader = async () => {
@@ -36,7 +46,28 @@ export const clientLoader = async () => {
 const Dashboard = ({ loaderData }: Route.ComponentProps ) => {
 
     const user = loaderData.user as User | null;
-    const { dashboardStats, allTrips, userGrowth, tripsByTravelStyle, allUsers } = loaderData
+    const { dashboardStats, allTrips, userGrowth, tripsByTravelStyle, allUsers } = loaderData;
+
+    const trips = allTrips.map((trip) => ({
+        imageUrl: trip.imageUrls[0],
+        name: trip.name,
+        interest: trip.interests
+    }))
+
+    const userAndTrips = [
+        {
+            title: "Latest user signups",
+            dataSource: allUsers,
+            field: "count",
+            headerText: "Trips created"
+        },
+        {
+            title: "Trips based on interests",
+            dataSource: trips,
+            field: "interest",
+            headerText: "Interests"
+        }
+    ]
 
     return (
     <main className="dashboard wrapper">
@@ -84,7 +115,97 @@ const Dashboard = ({ loaderData }: Route.ComponentProps ) => {
                 ))}
             </div>
         </section>
-        {/*<TripCard />*/}
+
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <ChartComponent
+                id="chart-1"
+                primaryXAxis={userXAxis}
+                primaryYAxis={useryAxis}
+                title="User Growth"
+                tooltip={{enable: true}}
+            >
+                {/* fitur chart*/}
+                <Inject services={[ColumnSeries, SplineAreaSeries, Category, DataLabel, Tooltip]} />
+
+                {/* data chart*/}
+                <SeriesCollectionDirective>
+
+                    <SeriesDirective // ini adalah diagram nya
+                        dataSource={userGrowth}
+                        xName="day" // x name : diisi dengan userGrowth.day
+                        yName="count" // y name : diisi dengan userGrowth.count
+                        type="Column" // tipe : diagram histogram
+                        name="Column"
+                        columnWidth={0.3}
+                        cornerRadius={{topLeft: 10, topRight: 10}}
+                    />
+
+                    <SeriesDirective
+                        dataSource={userGrowth}
+                        xName="day" // x name : diisi dengan userGrowth.day
+                        yName="count" // y name : diisi dengan userGrowth.count
+                        type="SplineArea" // tipe : diagram waves
+                        name="Wave"
+                        fill="rgba(71, 132, 238, 0.3)"
+                        columnWidth={0.3}
+                        cornerRadius={{topLeft: 10, topRight: 10}}
+                        border={{width: 2, color: "#4784EE"}}
+                    />
+                </SeriesCollectionDirective>
+            </ChartComponent>
+
+            <ChartComponent
+                id="chart-2"
+                primaryXAxis={tripXAxis}
+                primaryYAxis={tripyAxis}
+                title="Trip Trends"
+                tooltip={{enable: true}}
+            >
+
+                <Inject services={[ColumnSeries, SplineAreaSeries, Category, DataLabel, Tooltip]} />
+
+                <SeriesCollectionDirective>
+
+                    <SeriesDirective
+                        dataSource={tripsByTravelStyle}
+                        xName="travelStyle"  // dari : tripsByTravelStyle.travelStyle
+                        yName="count" // dari : tripsByTravelStyle.count
+                        type="Column"
+                        name="day"
+                        columnWidth={0.3}
+                        cornerRadius={{topLeft: 10, topRight: 10}}
+                    />
+
+                </SeriesCollectionDirective>
+            </ChartComponent>
+        </section>
+
+        <section className="user-trip wrapper">
+            {userAndTrips.map(({title, dataSource, field, headerText}, i) => (
+                <div key={i} className="flex flex-col gap-5">
+                    <h3 className="p-20-semibold text-dark-100">{title}</h3>
+                    <GridComponent dataSource={dataSource} gridLines="None">
+                        <ColumnsDirective>
+                            <ColumnDirective
+                                field="name"
+                                headerText="Name"
+                                width="200"
+                                textAlign="Left"
+                                template={(props: UserData) => (
+                                    <div className="flex items-center gap-1.5 px-4">
+                                        <img src={props.imageUrl} alt="users" className="rounded-full size-8 aspect-square" referrerPolicy="no-referrer" />
+                                        <span>{props.name}</span>
+                                    </div>
+                                )}
+                            />
+
+                            <ColumnDirective field={field} headerText={headerText} width="150" textAlign="left" />
+                        </ColumnsDirective>
+                    </GridComponent>
+                </div>
+                ))}
+        </section>
+
     </main>
   );
 };
